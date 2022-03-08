b5ffdcfa9e45d8e3b06b5401b366f72cb2982b40
Adrian Pelayo <niniohhns@gmail.com>
2022-03-08 14:08:26 +0100

    Fetch API (JSON) + Styled index

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const requestUrl = "./api/peliculas.json";
+const requestUrl = "http://localhost:3000/peliculas";
 const movieSection = document.getElementById('movieSection')
 
 
@@ -7,26 +7,38 @@ const getMovies = async () => {
     const movies = await response.json();
     return movies
 }
+
+const deleteMovie = async (id) => {
+
+}
+
 getMovies()
-        .then((movies) => {
+        .then(movies => {
             console.log(movies);
             
-            movies.peliculas.forEach(movie => {
+            movies.forEach(movie => {
+
+                let id = movie.id;
+                let title = movie.nombre;
+                let director = movie.director;
+                let clasification = movie.clasificacion;
+                let img = movie.imgUrl ?? '';
 
 
                 movieSection.innerHTML += `
-                <div class="card">
-                    <img src=${img} alt="${title} Image" />
-                    <div class="card-body">
-                        <h3 class="card-title">${title}</h3>
-                        <h4 class="card-subtitle">Director: <span class="bold">${director}</span></h4>
-                        <a href="#" class="btn btn-primary"></a>
+                <section id="${id}" class="card col-12 col-md-4 col-xl-3 card-movie">
+                    <div class="card-header text-center py-2">
+                        <h4 class="card-title">${title}</h4>
                     </div>
-                </div>
+                    <div class="card-body" style="background-image:url(${img})">
+                    </div>
+                    <div class="card-footer text-muted">
+                        <h5 class="card-subtitle mt-4 mb-2">${clasification}</h5>
+                        <h6 class="card-subtitle mb-4">${director}</h6>
+                    </div>
+                </section>
                 `
             })
+        }).catch(error => {
+            console.error(error);
         })
-
-function deleteMovie(id) {
-
-}
